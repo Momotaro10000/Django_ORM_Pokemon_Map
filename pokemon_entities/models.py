ee41d244beebc4db587f65c47bce4ad788222f78
Momotaro10000 <nakanomei@gmail.com>
2022-02-09 17:28:46 +0300

    added previous evolution

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,13 +7,19 @@ class Pokemon(models.Model):
     title_jp = models.CharField(max_length=200, null=True, verbose_name='Название по-японски')
     image = models.ImageField(upload_to='pokemons', blank=True, null=True, verbose_name='Картинка')
     description = models.TextField(blank=True, verbose_name='Описание')
+    previous_evolution = models.ForeignKey('self',
+                                           verbose_name='Из кого эволюционировал',
+                                           null=True,
+                                           blank=True,
+                                           on_delete=models.SET_NULL,
+                                           related_name='next_evolutions')
 
     def __str__(self):
         return '{}'.format(self.title_ru)
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name='pokemon_entities')
+    pokemon = models.ForeignKey('Pokemon', on_delete=models.CASCADE, related_name='entities')
     lat = models.FloatField()
     lon = models.FloatField()
     appeared_at = models.DateTimeField(null=True)
